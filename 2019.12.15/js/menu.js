9a8e8550d378cc4534cebcbba0928e46744531d2
azimut-mytishi <53815601+azimut-mytishi@users.noreply.github.com>
2019-12-15 13:08:11 +0300

    Add files via upload

---
+++ 2019.12.15/js/menu.js
@@ -0,0 +1,8 @@
+"use strict";
+document.write('<div class="menu">')
+document.write('    <ul> ')
+document.write('        <a class="menuli" href="https://azimut-mytishi.github.io/">Главная</a>')
+document.write('        <a class="menuli" href="https://azimut-mytishi.github.io/information.html">О нас</a> ')
+document.write('        <a class="menuli" href="https://azimut-mytishi.github.io/kontacts.html">Контакты</a>')
+document.write('        <a class="menuli none-border" href="https://azimut-mytishi.github.io/archive.html">Архив карт</a> </ul>')
+document.write('</div>')
